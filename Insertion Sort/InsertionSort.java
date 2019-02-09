a3207913b25f998422cc536f982c1a9073e01da5
Ryan <azbo400@gmail.com>
2019-02-09 11:36:58 -0800

    Fixed up some code

--- Insertion Sort/InsertionSort.java
+++ Insertion Sort/InsertionSort.java
@@ -1,17 +1,16 @@
 public class InsertionSort {
   public void Swap(int[] arr) {
-    // could the swap method be the problem
     // start array at 1 because there is nothing to compare 0 
     for(int j = 1; j < arr.length; j++){
       int key = arr[j];
       int i = j - 1; 
       // move elements that are greater than key one position ahead of their current position
-      while (i > 0 && arr[i] > key) {
+      while (i >= 0 && arr[i] > key) {
         // move index back to swap element
         arr[i + 1] = arr[i];
         i = i - 1;
       }
-      // once completed move index forward to keep moving through arary
+      // once completed move index forward to keep moving through arary and continue looping through
       arr[i + 1] = key;
     }
 
@@ -27,7 +26,7 @@ public class InsertionSort {
   }
 
   public static void main(String args[]) {
-    int[] arr = {1, 2, 4, 5, 3, 6};
+    int[] arr = {3, 2, 1, 4, 6, 5};
     InsertionSort a = new InsertionSort();
     a.Swap(arr);
     a.PrintArray(arr);
